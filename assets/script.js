5ccf6abd38b427a3bc94aef27f89758f4e7a5a33
jillo <jill.l.oreilly@gmail.com>
2023-12-19 12:10:58 +0000

    Remove pseudocode from js

--- assets/script.js
+++ assets/script.js
@@ -92,27 +92,9 @@ const upperCasedCharacters = [
 let pwdLength;
 let possibleChar = [];
 let guaranteedChar = [];
-// You can store the generatedPassword as a string and concat each character OR
-// !as an array and push each character, then join once you have enough characters
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-
-  // !Prompt for password length
-  // !At least 8 characters, no more than 128 characters
-  // !Conditional to check that the number that was entered is in range
-  // !Prompts store data as strings, so need to parse into a number
-  // !If the user's input is out of range, either return out of the function or call the function again
-
-  // !Confirm which character sets to use
-  // !If the user answers false for all, either return out of the function or call the function again
-  
-  // !Once they select a character set:
-  // !Generate a random character for each selected character set
-  // !Either push selected character sets to a mega-array of all selected characters
-  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-  
-  // !Once character sets are selected, move on to generating random characters
 
   possibleChar = []; // Reset array to empty
   guaranteedChar = [];
@@ -130,6 +112,7 @@ function getPasswordOptions() {
     return getPasswordOptions(); 
   }
   
+  // Ask user to select character sets
   let specialChar = confirm("Would you like special characters in your password?");
   let numericChar = confirm("Would you like numeric characters in your password?");
   let upperCaseChar = confirm("Would you like upper case characters in your password?");
@@ -203,18 +186,6 @@ function generatePassword() {
   const generatedPwdArr = []; // Declare variables inside function to prevent passwords appending when button is clicked again
   let generatedPwd = '';
   let options = getPasswordOptions();
-
- // Add a for loop here
- // Need a variable to hold the password as it's being generated
-  // Need a variable to hold the index that's being generated
-
-  // For loop that loops the number of times that matches the length the user chose
-  // Generate a random number
-  // That number is the index for a character in the mega-array
-  // So then, mega-array[generated-index] is the actual character
-  // Add that character to the password
-
-  // Once we finish the for loop, return the generated password
   
   for (let i = 0; i < pwdLength; i++) {
     let element = getRandomItem(possibleChar);
